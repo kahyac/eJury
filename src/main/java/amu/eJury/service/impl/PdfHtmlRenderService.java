eba8b30f384557bbf5a4e2f78d747348c55b6b88
macbook <ahmed.kartout@etu.univ-amu.fr>
2025-06-30 18:19:52 +0200

    PV signed and time-stamped

--- src/main/java/amu/eJury/service/impl/PdfHtmlRenderService.java
+++ src/main/java/amu/eJury/service/impl/PdfHtmlRenderService.java
@@ -10,7 +10,14 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.List;
+import java.util.Date;
+import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +36,20 @@ public class PdfHtmlRenderService {
         context.setVariable("semestrialResults", semestrialResults);
         context.setVariable("annualResults", annualResults);
         context.setVariable("yearResult", yearResult);
+        context.setVariable("now", Date.from(LocalDateTime.now()
+                .atZone(ZoneId.systemDefault()).toInstant()));
+        String signatureBase64 = "";
+        try {
+            Path path = Path.of("src/main/resources/static/images/logo-signature.png");
+            byte[] imageBytes = Files.readAllBytes(path);
+            signatureBase64 = Base64.getEncoder().encodeToString(imageBytes);
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture de l'image de signature : " + e.getMessage());
+        }
+        System.out.println("Signature Base64 length: " + signatureBase64.length());
+        System.out.println("Signature preview: " + signatureBase64.substring(0, 30) + "...");
+
+        context.setVariable("signatureBase64", signatureBase64);
 
         return templateEngine.process("pv-template", context);
     }
